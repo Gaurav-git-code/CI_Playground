8d56ae127bdf4a61ae79f71cdf1a9ef3dd2cc368
Gaurav Suman <gauravsuman2018@gmail.com>
2026-04-25 11:19:41 +0200

    CI playground app.

---
+++ src/main/java/com/darknightcoder/CiPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.darknightcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiPlaygroundApplication.class, args);
+	}
+
+}
